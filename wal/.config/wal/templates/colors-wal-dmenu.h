cf59a253cad73c9a7c108dc76faaf56d9964333f
salastro <salah2112004@gmail.com>
2025-02-03 09:42:10 +0200

    Add pywal

---
+++ wal/.config/wal/templates/colors-wal-dmenu.h
@@ -0,0 +1,8 @@
+static const char *colors[SchemeLast][2] = {{
+	/*     fg         bg       */
+	[SchemeNorm] =		{{ "{color15}", "{color0}" }},
+	[SchemeSel] =		{{ "{color15}", "{color1}" }},
+	[SchemeSelHighlight] = 	{{ "{color6}", "{color1}" }},
+	[SchemeNormHighlight] =	{{ "{color6}", "{color0}" }},
+	[SchemeOut] = 		{{ "{color15}", "{color14}" }},
+}};
---
+++ wal/.config/wal/templates/colors-wal-slock.h
@@ -0,0 +1,6 @@
+static const char *colorname[NUMCOLS] = {{
+	[BACKGROUND] =   "{color0}",     /* after initialization */
+	[INIT] =         "{color8}",     /* after initialization */
+	[INPUT] =        "{color4}",   /* during input */
+	[FAILED] =       "{color1}",   /* wrong password */
+}};
